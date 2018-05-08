c34a63553c0049d78bdb60e5170cd078eac42a07
Angelina Wong <angelina.w25@gmail.com>
2018-05-08 14:45:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-=begin
+
 def sentence(reversed_words)
-  return reversed_words.join('')
+  return reversed_words.join(' ')
 end
 
 def reverse_each_word(sentence)
-  reversed_words = sentence.split("")
+  reversed_words = sentence.split(" ")
   reversed_words.collect do |word|
     word.reverse!
 
@@ -14,17 +14,4 @@ end
 return sentence(reversed_words)
 
 end
-=end
 
-def join_string(list)
-  return list.join(' ')
-end
-
-def reverse_each_word(sentence)
-  list = sentence.split(" ")
-  list.collect do |word|
-    word.reverse!
-  end
-
-  return join_string(list)
-end

3c9c2cd30fe0c7bee9d6fe9c108c10f449c280c3
Angelina Wong <angelina.w25@gmail.com>
2018-05-08 14:45:57 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+
+def sentence(reversed_words)
+  return reversed_words.join(' ')
+end
+
+def reverse_each_word(sentence)
+  reversed_words = sentence.split(" ")
+  reversed_words.collect do |word|
+    word.reverse!
+
+
+end
+
+return sentence(reversed_words)
+
+end
